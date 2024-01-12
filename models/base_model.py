464878c69877e4d2fe6c9ce4656a7d72f9377ce4
umumararungu <umcynthi@gmail.com>
2024-01-12 15:39:42 +0200

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,21 @@ import uuid
 from datetime import datetime
  
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """new instances"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        time_format = "%Y-%m-%dT%H:%M:%s.%f"
+        if kwargs:
+            for key,value in kwargs2.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self,key,datetime.strptime(value,time_format))
+                else:
+                    setattr(self,key,value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
     
     def save(self):
         """
