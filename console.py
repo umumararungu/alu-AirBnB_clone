6b862eb353233dd15050bd469206ffd4ff654702
angep72 <p.umunyana@alustudent.com>
2024-01-16 03:00:19 -0800

    console file

--- console.py
+++ console.py
@@ -1,31 +1,26 @@
-#!/usr/bin/python3
-"""
-console file
-"""
+#!/usr/bin/env python3
+"""Module for console.py"""
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Cmd
-    """
-    prompt = "(hbnb)"
+    """Command interpreter for HBNB project"""
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
 
     def do_quit(self, arg):
-        """
-        """
+        """Quit command to exit the program"""
         return True
-    
-    def help_quit(self, arg):
-        """
-        """
-        print("Quit to exit program")
-        
+
     def do_EOF(self, arg):
-        """
-        """
-        print("")
+        """EOF command to exit the program"""
+        print()
         return True
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
