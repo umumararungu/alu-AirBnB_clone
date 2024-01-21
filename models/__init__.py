ffb4a2d57d4730fe57ab2709ed74b92fada9a930
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-21 22:46:24 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,17 @@
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+"""
+initialize the models package
+"""
 
-storage = FileStorage()
+from os import getenv
+
+
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
