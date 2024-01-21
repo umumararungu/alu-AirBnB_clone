2a0ae39a389117e40e384e9f2590bdb0d641522e
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-21 21:51:19 +0200

    Update console.py

--- console.py
+++ console.py
@@ -4,15 +4,11 @@
 import cmd
 from datetime import datetime
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models import storage
 import shlex  # for splitting the line along spaces except in double quotes
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = { "BaseModel": BaseModel
+           }
 
 
 class HBNBCommand(cmd.Cmd):
